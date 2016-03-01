8c0bdbc71a4352705a47fdfb50f706d2cc2e8b0c
Ted Hu <hu1314v5@outlook.com>
2016-03-01 23:55:37 +0800

    home fragment 界面完成

--- app/src/main/java/com/hu131/houserental/MainActivity.java
+++ app/src/main/java/com/hu131/houserental/MainActivity.java
@@ -3,9 +3,14 @@ package com.hu131.houserental;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.RadioGroup;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.hu131.houserental.fragment.Search;
 import com.hu131.houserental.fragment.Home;
 import com.hu131.houserental.fragment.User;
@@ -17,13 +22,22 @@ public class MainActivity extends Activity {
     private User userFragment;
     private FragmentManager fragmentManager;
     private FragmentTransaction fragmentTransaction;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //透明状态栏(沉浸式状态栏)
+       // getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+
         setContentView(R.layout.activity_main);
         RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radio_group_main);
-
         fragmentManager = getFragmentManager();
         checkable(radioGroup.getCheckedRadioButtonId());//先显示默认的
 
@@ -33,6 +47,9 @@ public class MainActivity extends Activity {
                 checkable(checkedId);
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     /**
@@ -97,4 +114,43 @@ public class MainActivity extends Activity {
         }
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.hu131.houserental/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.hu131.houserental/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
